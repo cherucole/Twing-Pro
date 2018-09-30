62247fc34007f64c5d25983bae102aa9558ea865
cherucole <cheruyotcheruyot@gmail.com>
2018-09-30 21:03:58 +0300

    heroku deploy

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://cherucole:cherucole@localhost/twing'
+    # SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://cherucole:cherucole@localhost/twing'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
@@ -23,13 +23,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://cherucole:cherucole@localhost/twing'
